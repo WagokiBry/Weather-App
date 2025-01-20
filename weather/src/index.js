0ea7e125929c526e3c4b286289bd8c6a4bff28a3
CodingSwordfish <wagobryan@gmail.com>
2025-01-20 09:25:00 +0000

    Add a function to take location and return response object

--- weather/src/index.js
+++ weather/src/index.js
@@ -1,3 +1,32 @@
 import "./styles.css"
 
-console.log("hello world")
+async function searchAndDisplayWeather(location){
+    
+    try {
+      let fetchedData = await fetch(
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&include=days&key=BGMXWPTCJ9EFT7U9HKV3TXT2L&contentType=json`
+       ,
+        { mode: "cors" }
+      )
+
+      if (!fetchedData.ok) {
+        throw new Error(
+          `API request failed with status: ${response.status}`
+        );
+      }
+      let searchData = await fetchedData.json();
+console.log(searchData)
+
+      
+    }
+
+
+
+    catch (error) {
+      console.error("Error fetching GIF:", error);
+      // Handle the error here, e.g. display an error message to the user
+      // img.src = "sonic.jpg"; // Set a placeholder image
+    };
+  }
+
+  searchAndDisplayWeather("ottawa")
