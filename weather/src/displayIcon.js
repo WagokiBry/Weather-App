30f3b8eac24c588b283f51bec5ebf756194434d8
Peacefulkind <68757766+WagokiBry@users.noreply.github.com>
2025-01-21 12:11:06 +0000

    Update the displayIcon module to display the images

--- weather/src/displayIcon.js
+++ weather/src/displayIcon.js
@@ -1,15 +1,17 @@
-import "./cloudy.jpg"
+import partlyCloudy from './images/cloudy.jpg'; 
+
 
 export function displayIcon(data) {
     const firstDayData = data[0]; // Access the first object in the array
   
     // Access the icon property:
     const weatherIcon = firstDayData.icon;
+    console.log(weatherIcon)
     weatherIconContainer.innerHTML= ""
   
-    if (weatherIcon === "partly-cloudy-day") {
+    if (weatherIcon.includes("partly-cloudy-day")) {
       const imageElement = document.createElement('img');
-      imageElement.src = '/workspaces/Weather-App/Weather-App/weather/src/images/cloudy.jpg'; // Replace with the actual path to your image
+      imageElement.src = partlyCloudy;
       imageElement.alt = "Partly Cloudy";
   
       const weatherIconContainer = document.getElementById('weatherIconContainer');
